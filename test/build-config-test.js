245b8944bab38699f2f1587c0ba862cec79e03fb
Lucas Holmquist <lholmqui@redhat.com>
2017-05-26 13:42:09 -0400

    config-loader is now its own module

--- test/build-config-test.js
+++ test/build-config-test.js
@@ -4,29 +4,33 @@ const test = require('tape');
 const nock = require('nock');
 
 const openshiftRestClient = require('../');
+const openshiftConfigLoader = require('openshift-config-loader');
 const privates = require('../lib/private-map');
 
 test('find - build-config', (t) => {
   const settings = {
     configLocation: `${__dirname}/test-config`
   };
-  openshiftRestClient(settings).then((client) => {
-    t.equal(typeof client.buildconfigs.find, 'function', 'There is a find method on the buildconfigs object');
 
-    const clientConfig = privates.get(client).config;
-    const buildConfigName = 'nodejs-rest-http-s2i';
+  openshiftConfigLoader(settings).then((config) => {
+    openshiftRestClient(config).then((client) => {
+      t.equal(typeof client.buildconfigs.find, 'function', 'There is a find method on the buildconfigs object');
 
-    nock(clientConfig.cluster)
-      .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
-      .get(`/oapi/v1/namespaces/${clientConfig.context.namespace}/buildconfigs/${buildConfigName}`)
-      .reply(200, {kind: 'BuildConfig'});
+      const clientConfig = privates.get(client).config;
+      const buildConfigName = 'nodejs-rest-http-s2i';
 
-    const findResult = client.buildconfigs.find(buildConfigName).then((buildconfig) => {
-      t.equal(buildconfig.kind, 'BuildConfig', 'returns an object with BuildConfig');
-      t.end();
+      nock(clientConfig.cluster)
+        .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
+        .get(`/oapi/v1/namespaces/${clientConfig.context.namespace}/buildconfigs/${buildConfigName}`)
+        .reply(200, {kind: 'BuildConfig'});
+
+      const findResult = client.buildconfigs.find(buildConfigName).then((buildconfig) => {
+        t.equal(buildconfig.kind, 'BuildConfig', 'returns an object with BuildConfig');
+        t.end();
+      });
+
+      t.equal(findResult instanceof Promise, true, 'should return a Promise');
     });
-
-    t.equal(findResult instanceof Promise, true, 'should return a Promise');
   });
 });
 
@@ -34,30 +38,33 @@ test('find - build-config - not found', (t) => {
   const settings = {
     configLocation: `${__dirname}/test-config`
   };
-  openshiftRestClient(settings).then((client) => {
-    const clientConfig = privates.get(client).config;
-    const buildConfigName = 'nodejs-rest-http-s2i-not-here';
 
-    const mockedErrorMessage = {
-      kind: 'Status',
-      apiVersion: 'v1',
-      metadata: {},
-      status: 'Failure',
-      message: 'buildconfigs "nodejs-rest-http-s2i-not-here" not found',
-      reason: 'NotFound',
-      details: { name: 'nodejs-rest-http-s2i-not-here', kind: 'buildconfigs' },
-      code: 404
-    };
+  openshiftConfigLoader(settings).then((config) => {
+    openshiftRestClient(config).then((client) => {
+      const clientConfig = privates.get(client).config;
+      const buildConfigName = 'nodejs-rest-http-s2i-not-here';
 
-    nock(clientConfig.cluster)
-      .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
-      .get(`/oapi/v1/namespaces/${clientConfig.context.namespace}/buildconfigs/${buildConfigName}`)
-      .reply(200, mockedErrorMessage);
+      const mockedErrorMessage = {
+        kind: 'Status',
+        apiVersion: 'v1',
+        metadata: {},
+        status: 'Failure',
+        message: 'buildconfigs "nodejs-rest-http-s2i-not-here" not found',
+        reason: 'NotFound',
+        details: { name: 'nodejs-rest-http-s2i-not-here', kind: 'buildconfigs' },
+        code: 404
+      };
 
-    client.buildconfigs.find(buildConfigName).then((buildconfig) => {
-      t.equal(buildconfig.kind, 'Status', 'returns an object with Status');
-      t.equal(buildconfig.code, 404, 'code with a 404');
-      t.end();
+      nock(clientConfig.cluster)
+        .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
+        .get(`/oapi/v1/namespaces/${clientConfig.context.namespace}/buildconfigs/${buildConfigName}`)
+        .reply(200, mockedErrorMessage);
+
+      client.buildconfigs.find(buildConfigName).then((buildconfig) => {
+        t.equal(buildconfig.kind, 'Status', 'returns an object with Status');
+        t.equal(buildconfig.code, 404, 'code with a 404');
+        t.end();
+      });
     });
   });
 });
@@ -67,31 +74,33 @@ test('create a build-config', (t) => {
     configLocation: `${__dirname}/test-config`
   };
 
-  openshiftRestClient(settings).then((client) => {
-    t.equal(typeof client.buildconfigs.create, 'function', 'There is a create method on the buildconfigs object');
+  openshiftConfigLoader(settings).then((config) => {
+    openshiftRestClient(config).then((client) => {
+      t.equal(typeof client.buildconfigs.create, 'function', 'There is a create method on the buildconfigs object');
 
-    const clientConfig = privates.get(client).config;
-    const buildConfigName = 'nodejs-rest-http-s2i-test';
+      const clientConfig = privates.get(client).config;
+      const buildConfigName = 'nodejs-rest-http-s2i-test';
 
-    const buildConfig = {
-      apiVersion: 'v1',
-      kind: 'BuildConfig',
-      metadata: {
-        name: buildConfigName,
-        namespace: clientConfig.context.namespace
-      }
-    };
+      const buildConfig = {
+        apiVersion: 'v1',
+        kind: 'BuildConfig',
+        metadata: {
+          name: buildConfigName,
+          namespace: clientConfig.context.namespace
+        }
+      };
 
-    nock(clientConfig.cluster)
-      .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
-      .post(`/oapi/v1/namespaces/${clientConfig.context.namespace}/buildconfigs`)
-      .reply(201, buildConfig);
+      nock(clientConfig.cluster)
+        .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
+        .post(`/oapi/v1/namespaces/${clientConfig.context.namespace}/buildconfigs`)
+        .reply(201, buildConfig);
 
-    const createResult = client.buildconfigs.create(buildConfig).then((buildconfig) => {
-      t.equal(buildconfig.kind, 'BuildConfig', 'returns an object with BuildConfig');
-      t.end();
+      const createResult = client.buildconfigs.create(buildConfig).then((buildconfig) => {
+        t.equal(buildconfig.kind, 'BuildConfig', 'returns an object with BuildConfig');
+        t.end();
+      });
+
+      t.equal(createResult instanceof Promise, true, 'should return a Promise');
     });
-
-    t.equal(createResult instanceof Promise, true, 'should return a Promise');
   });
 });
--- test/projects-test.js
+++ test/projects-test.js
@@ -6,23 +6,26 @@ const test = require('tape');
 const nock = require('nock');
 
 const privates = require('../lib/private-map');
+const openshiftConfigLoader = require('openshift-config-loader');
 
 test('testing project endpoint', (t) => {
   const settings = {
     configLocation: `${__dirname}/test-config`
   };
 
-  openshiftRestClient(settings).then((client) => {
-    const clientConfig = privates.get(client).config;
+  openshiftConfigLoader(settings).then((config) => {
+    openshiftRestClient(config).then((client) => {
+      const clientConfig = privates.get(client).config;
 
-    nock(clientConfig.cluster)
-      .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
-      .get('/oapi/v1/projects')
-      .reply(200, {mocked: 'mocked'});
+      nock(clientConfig.cluster)
+        .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
+        .get('/oapi/v1/projects')
+        .reply(200, {mocked: 'mocked'});
 
-    client.projects.find().then((projects) => {
-      t.pass('return successful');
-      t.end();
+      client.projects.find().then((projects) => {
+        t.pass('return successful');
+        t.end();
+      });
     });
   });
 });
