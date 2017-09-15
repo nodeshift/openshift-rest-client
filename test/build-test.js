0ee1ee89f94d37b6caa801f690a9806053158870
Lucas Holmquist <lholmqui@redhat.com>
2017-09-15 18:30:10 +0000

    feat: add findAll and remove for builds

--- test/build-test.js
+++ test/build-test.js
@@ -33,3 +33,83 @@ test('find - build', (t) => {
     });
   });
 });
+
+test('findall - build', (t) => {
+  const settings = {
+    configLocation: `${__dirname}/test-config`
+  };
+
+  openshiftConfigLoader(settings).then((config) => {
+    openshiftRestClient(config).then((client) => {
+      t.equal(typeof client.builds.find, 'function', 'There is a find method on the build object');
+
+      const clientConfig = privates.get(client).config;
+
+      const buildList = {
+        kind: 'BuildList',
+        items: [
+          {
+            metadata: {
+              name: 'nodejs-rest-http-s2i-1'
+            }
+          }
+        ]
+      };
+
+      nock(clientConfig.cluster)
+        .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
+        .get(`/oapi/v1/namespaces/${clientConfig.context.namespace}/builds`)
+        .reply(200, buildList);
+
+      const findResult = client.builds.findAll().then((buildList) => {
+        t.equal(buildList.kind, 'BuildList', 'returns an object with Build');
+        t.equal(buildList.items[0].metadata.name, 'nodejs-rest-http-s2i-1', 'should have the build name');
+        t.end();
+      });
+
+      t.equal(findResult instanceof Promise, true, 'should return a Promise');
+    });
+  });
+});
+
+test('remove - build - basic remove', (t) => {
+  const settings = {
+    configLocation: `${__dirname}/test-config`
+  };
+
+  openshiftConfigLoader(settings).then((config) => {
+    openshiftRestClient(config).then((client) => {
+      t.equal(typeof client.builds.remove, 'function', 'There is a remove method on the build object');
+
+      const clientConfig = privates.get(client).config;
+      const buildName = 'cool-deployment-name-1';
+
+      nock(clientConfig.cluster)
+        .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
+        .delete(`/oapi/v1/namespaces/${clientConfig.context.namespace}/builds/${buildName}`)
+        .reply(200, {kind: 'Status'});
+
+      const removeResult = client.builds.remove(buildName).then((status) => {
+        t.equal(status.kind, 'Status', 'returns an object with Status');
+        t.end();
+      });
+
+      t.equal(removeResult instanceof Promise, true, 'should return a Promise');
+    });
+  });
+});
+
+test('remove - build - remove - no build name', (t) => {
+  const settings = {
+    configLocation: `${__dirname}/test-config`
+  };
+
+  openshiftConfigLoader(settings).then((config) => {
+    openshiftRestClient(config).then((client) => {
+      client.builds.remove().catch((err) => {
+        t.equal(err.message, 'Build Name is required', 'error message should return');
+        t.end();
+      });
+    });
+  });
+});
