5564e653e77b8718593954cd86663d4007aa5072
Tom Vogel <tom.vogel@telus.com>
2018-01-24 21:42:34 -0800

    test(services): add tests for services

--- test/services-test.js
+++ test/services-test.js
@@ -7,7 +7,33 @@ const openshiftRestClient = require('../');
 const openshiftConfigLoader = require('openshift-config-loader');
 const privates = require('../lib/private-map');
 
-test('find - services', (t) => {
+test('find - services - basic findAll', (t) => {
+  const settings = {
+    configLocation: `${__dirname}/test-config`
+  };
+
+  openshiftConfigLoader(settings).then((config) => {
+    openshiftRestClient(config).then((client) => {
+      t.equal(typeof client.services.findAll, 'function', 'There is a findAll method on the services object');
+
+      const clientConfig = privates.get(client).config;
+
+      nock(clientConfig.cluster)
+        .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
+        .get(`/api/v1/namespaces/${clientConfig.context.namespace}/services`)
+        .reply(200, {kind: 'ServiceList'});
+
+      const findResult = client.services.findAll().then((serviceList) => {
+        t.equal(serviceList.kind, 'ServiceList', 'returns an object with ServiceList');
+        t.end();
+      });
+
+      t.equal(findResult instanceof Promise, true, 'should return a Promise');
+    });
+  });
+});
+
+test('find - services - basic find', (t) => {
   const settings = {
     configLocation: `${__dirname}/test-config`
   };
@@ -17,14 +43,14 @@ test('find - services', (t) => {
       t.equal(typeof client.services.find, 'function', 'There is a find method on the services object');
 
       const clientConfig = privates.get(client).config;
-      const servicesName = 'nodejs-rest-http';
+      const serviceName = 'cool-service-name-1';
 
       nock(clientConfig.cluster)
         .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
-        .get(`/api/v1/namespaces/${clientConfig.context.namespace}/services/${servicesName}`)
+        .get(`/api/v1/namespaces/${clientConfig.context.namespace}/services/${serviceName}`)
         .reply(200, {kind: 'Service'});
 
-      const findResult = client.services.find(servicesName).then((service) => {
+      const findResult = client.services.find(serviceName).then((service) => {
         t.equal(service.kind, 'Service', 'returns an object with Service');
         t.end();
       });
@@ -34,7 +60,22 @@ test('find - services', (t) => {
   });
 });
 
-test('create - services', (t) => {
+test('find - services - find - no service name', (t) => {
+  const settings = {
+    configLocation: `${__dirname}/test-config`
+  };
+
+  openshiftConfigLoader(settings).then((config) => {
+    openshiftRestClient(config).then((client) => {
+      client.services.find().catch((err) => {
+        t.equal(err.message, 'Service Name is required', 'error message should return');
+        t.end();
+      });
+    });
+  });
+});
+
+test('create - service', (t) => {
   const settings = {
     configLocation: `${__dirname}/test-config`
   };
@@ -63,6 +104,62 @@ test('create - services', (t) => {
   });
 });
 
+test('update - service', (t) => {
+  const settings = {
+    configLocation: `${__dirname}/test-config`
+  };
+
+  openshiftConfigLoader(settings).then((config) => {
+    openshiftRestClient(config).then((client) => {
+      t.equal(typeof client.services.create, 'function', 'There is a create method on the services object');
+
+      const clientConfig = privates.get(client).config;
+      const service = {
+        kind: 'Service'
+      };
+      const serviceName = 'cool-service-name-1';
+
+      nock(clientConfig.cluster)
+        .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
+        .put(`/api/v1/namespaces/${clientConfig.context.namespace}/services/${serviceName}`)
+        .reply(200, {kind: 'Service'});
+
+      const createResult = client.services.update(serviceName, service).then((service) => {
+        t.equal(service.kind, 'Service', 'returns an object with Service');
+        t.end();
+      });
+
+      t.equal(createResult instanceof Promise, true, 'should return a Promise');
+    });
+  });
+});
+
+test('remove - services - basic removeAll', (t) => {
+  const settings = {
+    configLocation: `${__dirname}/test-config`
+  };
+
+  openshiftConfigLoader(settings).then((config) => {
+    openshiftRestClient(config).then((client) => {
+      t.equal(typeof client.services.removeAll, 'function', 'There is a removeAll method on the services object');
+
+      const clientConfig = privates.get(client).config;
+
+      nock(clientConfig.cluster)
+        .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
+        .delete(`/api/v1/namespaces/${clientConfig.context.namespace}/services`)
+        .reply(200, {kind: 'Status'});
+
+      const removeResult = client.services.removeAll().then((serviceList) => {
+        t.equal(serviceList.kind, 'Status', 'returns an object with Status');
+        t.end();
+      });
+
+      t.equal(removeResult instanceof Promise, true, 'should return a Promise');
+    });
+  });
+});
+
 test('remove - services - basic remove', (t) => {
   const settings = {
     configLocation: `${__dirname}/test-config`
@@ -73,7 +170,7 @@ test('remove - services - basic remove', (t) => {
       t.equal(typeof client.services.remove, 'function', 'There is a remove method on the services object');
 
       const clientConfig = privates.get(client).config;
-      const serviceName = 'cool-service-name';
+      const serviceName = 'cool-service-name-1';
 
       nock(clientConfig.cluster)
         .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
@@ -90,7 +187,7 @@ test('remove - services - basic remove', (t) => {
   });
 });
 
-test('remove - services - remove - no rep name', (t) => {
+test('remove - services - remove - no service name', (t) => {
   const settings = {
     configLocation: `${__dirname}/test-config`
   };
