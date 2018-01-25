bf05681fc86b7a01f8fc1a85723b8296ee3babb9
Tom Vogel <tom.vogel@telus.com>
2018-01-24 21:31:57 -0800

    test(deployment-config): add tests for deploymentconfigs

--- test/deployment-config-test.js
+++ test/deployment-config-test.js
@@ -7,7 +7,33 @@ const openshiftRestClient = require('../');
 const openshiftConfigLoader = require('openshift-config-loader');
 const privates = require('../lib/private-map');
 
-test('find - deployment-config', (t) => {
+test('find - deploymentconfigs - basic findAll', (t) => {
+  const settings = {
+    configLocation: `${__dirname}/test-config`
+  };
+
+  openshiftConfigLoader(settings).then((config) => {
+    openshiftRestClient(config).then((client) => {
+      t.equal(typeof client.deploymentconfigs.findAll, 'function', 'There is a findAll method on the deploymentconfigs object');
+
+      const clientConfig = privates.get(client).config;
+
+      nock(clientConfig.cluster)
+        .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
+        .get(`/oapi/v1/namespaces/${clientConfig.context.namespace}/deploymentconfigs`)
+        .reply(200, {kind: 'DeploymentConfigList'});
+
+      const findResult = client.deploymentconfigs.findAll().then((deploymentConfigList) => {
+        t.equal(deploymentConfigList.kind, 'DeploymentConfigList', 'returns an object with DeploymentConfigList');
+        t.end();
+      });
+
+      t.equal(findResult instanceof Promise, true, 'should return a Promise');
+    });
+  });
+});
+
+test('find - deploymentconfigs - basic find', (t) => {
   const settings = {
     configLocation: `${__dirname}/test-config`
   };
@@ -17,14 +43,14 @@ test('find - deployment-config', (t) => {
       t.equal(typeof client.deploymentconfigs.find, 'function', 'There is a find method on the deploymentconfigs object');
 
       const clientConfig = privates.get(client).config;
-      const deploymentconfigName = 'nodejs-rest-http';
+      const deploymentConfigName = 'cool-deploymentconfig-name-1';
 
       nock(clientConfig.cluster)
         .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
-        .get(`/oapi/v1/namespaces/${clientConfig.context.namespace}/deploymentconfigs/${deploymentconfigName}`)
+        .get(`/oapi/v1/namespaces/${clientConfig.context.namespace}/deploymentconfigs/${deploymentConfigName}`)
         .reply(200, {kind: 'DeploymentConfig'});
 
-      const findResult = client.deploymentconfigs.find(deploymentconfigName).then((deploymentconfig) => {
+      const findResult = client.deploymentconfigs.find(deploymentConfigName).then((deploymentconfig) => {
         t.equal(deploymentconfig.kind, 'DeploymentConfig', 'returns an object with DeploymentConfig');
         t.end();
       });
@@ -34,17 +60,32 @@ test('find - deployment-config', (t) => {
   });
 });
 
-test('create - deployment-config', (t) => {
+test('find - deploymentconfigs - find - no deploymentconfig name', (t) => {
   const settings = {
     configLocation: `${__dirname}/test-config`
   };
 
   openshiftConfigLoader(settings).then((config) => {
     openshiftRestClient(config).then((client) => {
-      t.equal(typeof client.deploymentconfigs.find, 'function', 'There is a find method on the deploymentconfigs object');
+      client.deploymentconfigs.find().catch((err) => {
+        t.equal(err.message, 'Deployment Config Name is required', 'error message should return');
+        t.end();
+      });
+    });
+  });
+});
+
+test('create - deploymentconfig', (t) => {
+  const settings = {
+    configLocation: `${__dirname}/test-config`
+  };
+
+  openshiftConfigLoader(settings).then((config) => {
+    openshiftRestClient(config).then((client) => {
+      t.equal(typeof client.deploymentconfigs.create, 'function', 'There is a create method on the deploymentconfigs object');
 
       const clientConfig = privates.get(client).config;
-      const deploymentConfig = {
+      const deploymentconfig = {
         kind: 'DeploymentConfig'
       };
 
@@ -53,7 +94,7 @@ test('create - deployment-config', (t) => {
         .post(`/oapi/v1/namespaces/${clientConfig.context.namespace}/deploymentconfigs`)
         .reply(200, {kind: 'DeploymentConfig'});
 
-      const createResult = client.deploymentconfigs.create(deploymentConfig).then((deploymentconfig) => {
+      const createResult = client.deploymentconfigs.create(deploymentconfig).then((deploymentconfig) => {
         t.equal(deploymentconfig.kind, 'DeploymentConfig', 'returns an object with DeploymentConfig');
         t.end();
       });
@@ -63,7 +104,63 @@ test('create - deployment-config', (t) => {
   });
 });
 
-test('remove - deployment config - basic remove', (t) => {
+test('update - deploymentconfig', (t) => {
+  const settings = {
+    configLocation: `${__dirname}/test-config`
+  };
+
+  openshiftConfigLoader(settings).then((config) => {
+    openshiftRestClient(config).then((client) => {
+      t.equal(typeof client.deploymentconfigs.create, 'function', 'There is a create method on the deploymentconfigs object');
+
+      const clientConfig = privates.get(client).config;
+      const deploymentconfig = {
+        kind: 'DeploymentConfig'
+      };
+      const deploymentConfigName = 'cool-deploymentconfig-name-1';
+
+      nock(clientConfig.cluster)
+        .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
+        .put(`/oapi/v1/namespaces/${clientConfig.context.namespace}/deploymentconfigs/${deploymentConfigName}`)
+        .reply(200, {kind: 'DeploymentConfig'});
+
+      const createResult = client.deploymentconfigs.update(deploymentConfigName, deploymentconfig).then((deploymentconfig) => {
+        t.equal(deploymentconfig.kind, 'DeploymentConfig', 'returns an object with DeploymentConfig');
+        t.end();
+      });
+
+      t.equal(createResult instanceof Promise, true, 'should return a Promise');
+    });
+  });
+});
+
+test('remove - deploymentconfigs - basic removeAll', (t) => {
+  const settings = {
+    configLocation: `${__dirname}/test-config`
+  };
+
+  openshiftConfigLoader(settings).then((config) => {
+    openshiftRestClient(config).then((client) => {
+      t.equal(typeof client.deploymentconfigs.removeAll, 'function', 'There is a removeAll method on the deploymentconfigs object');
+
+      const clientConfig = privates.get(client).config;
+
+      nock(clientConfig.cluster)
+        .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
+        .delete(`/oapi/v1/namespaces/${clientConfig.context.namespace}/deploymentconfigs`)
+        .reply(200, {kind: 'Status'});
+
+      const removeResult = client.deploymentconfigs.removeAll().then((deploymentConfigList) => {
+        t.equal(deploymentConfigList.kind, 'Status', 'returns an object with Status');
+        t.end();
+      });
+
+      t.equal(removeResult instanceof Promise, true, 'should return a Promise');
+    });
+  });
+});
+
+test('remove - deploymentconfigs - basic remove', (t) => {
   const settings = {
     configLocation: `${__dirname}/test-config`
   };
@@ -73,7 +170,7 @@ test('remove - deployment config - basic remove', (t) => {
       t.equal(typeof client.deploymentconfigs.remove, 'function', 'There is a remove method on the deploymentconfigs object');
 
       const clientConfig = privates.get(client).config;
-      const deploymentConfigName = 'cool-deployment-name';
+      const deploymentConfigName = 'cool-deploymentconfig-name-1';
 
       nock(clientConfig.cluster)
         .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
@@ -90,7 +187,7 @@ test('remove - deployment config - basic remove', (t) => {
   });
 });
 
-test('remove - deployment config - remove - no rep name', (t) => {
+test('remove - deploymentconfigs - remove - no deploymentconfig name', (t) => {
   const settings = {
     configLocation: `${__dirname}/test-config`
   };
