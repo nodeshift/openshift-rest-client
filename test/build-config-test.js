8957399211977194f0b928f5e4bbf398a6f130f3
Tom Vogel <tom.vogel@telus.com>
2018-01-24 21:22:09 -0800

    test(build-configs): add tests for buildconfigs

--- test/build-config-test.js
+++ test/build-config-test.js
@@ -7,7 +7,33 @@ const openshiftRestClient = require('../');
 const openshiftConfigLoader = require('openshift-config-loader');
 const privates = require('../lib/private-map');
 
-test('find - build-config', (t) => {
+test('find - buildconfigs - basic findAll', (t) => {
+  const settings = {
+    configLocation: `${__dirname}/test-config`
+  };
+
+  openshiftConfigLoader(settings).then((config) => {
+    openshiftRestClient(config).then((client) => {
+      t.equal(typeof client.buildconfigs.findAll, 'function', 'There is a findAll method on the buildconfigs object');
+
+      const clientConfig = privates.get(client).config;
+
+      nock(clientConfig.cluster)
+        .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
+        .get(`/oapi/v1/namespaces/${clientConfig.context.namespace}/buildconfigs`)
+        .reply(200, {kind: 'BuildConfigList'});
+
+      const findResult = client.buildconfigs.findAll().then((buildConfigList) => {
+        t.equal(buildConfigList.kind, 'BuildConfigList', 'returns an object with BuildConfigList');
+        t.end();
+      });
+
+      t.equal(findResult instanceof Promise, true, 'should return a Promise');
+    });
+  });
+});
+
+test('find - buildconfigs - basic find', (t) => {
   const settings = {
     configLocation: `${__dirname}/test-config`
   };
@@ -17,7 +43,7 @@ test('find - build-config', (t) => {
       t.equal(typeof client.buildconfigs.find, 'function', 'There is a find method on the buildconfigs object');
 
       const clientConfig = privates.get(client).config;
-      const buildConfigName = 'nodejs-rest-http-s2i';
+      const buildConfigName = 'cool-buildconfig-name-1';
 
       nock(clientConfig.cluster)
         .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
@@ -34,42 +60,22 @@ test('find - build-config', (t) => {
   });
 });
 
-test('find - build-config - not found', (t) => {
+test('find - buildconfigs - find - no buildconfig name', (t) => {
   const settings = {
     configLocation: `${__dirname}/test-config`
   };
 
   openshiftConfigLoader(settings).then((config) => {
     openshiftRestClient(config).then((client) => {
-      const clientConfig = privates.get(client).config;
-      const buildConfigName = 'nodejs-rest-http-s2i-not-here';
-
-      const mockedErrorMessage = {
-        kind: 'Status',
-        apiVersion: 'v1',
-        metadata: {},
-        status: 'Failure',
-        message: 'buildconfigs "nodejs-rest-http-s2i-not-here" not found',
-        reason: 'NotFound',
-        details: { name: 'nodejs-rest-http-s2i-not-here', kind: 'buildconfigs' },
-        code: 404
-      };
-
-      nock(clientConfig.cluster)
-        .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
-        .get(`/oapi/v1/namespaces/${clientConfig.context.namespace}/buildconfigs/${buildConfigName}`)
-        .reply(404, mockedErrorMessage);
-
-      client.buildconfigs.find(buildConfigName).then((buildconfig) => {
-        t.equal(buildconfig.kind, 'Status', 'returns an object with Status');
-        t.equal(buildconfig.code, 404, 'code with a 404');
+      client.buildconfigs.find().catch((err) => {
+        t.equal(err.message, 'Build Config Name is required', 'error message should return');
         t.end();
       });
     });
   });
 });
 
-test('create a build-config', (t) => {
+test('create - buildconfig', (t) => {
   const settings = {
     configLocation: `${__dirname}/test-config`
   };
@@ -79,23 +85,16 @@ test('create a build-config', (t) => {
       t.equal(typeof client.buildconfigs.create, 'function', 'There is a create method on the buildconfigs object');
 
       const clientConfig = privates.get(client).config;
-      const buildConfigName = 'nodejs-rest-http-s2i-test';
-
-      const buildConfig = {
-        apiVersion: 'v1',
-        kind: 'BuildConfig',
-        metadata: {
-          name: buildConfigName,
-          namespace: clientConfig.context.namespace
-        }
+      const buildconfig = {
+        kind: 'BuildConfig'
       };
 
       nock(clientConfig.cluster)
         .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
         .post(`/oapi/v1/namespaces/${clientConfig.context.namespace}/buildconfigs`)
-        .reply(201, buildConfig);
+        .reply(200, {kind: 'BuildConfig'});
 
-      const createResult = client.buildconfigs.create(buildConfig).then((buildconfig) => {
+      const createResult = client.buildconfigs.create(buildconfig).then((buildconfig) => {
         t.equal(buildconfig.kind, 'BuildConfig', 'returns an object with BuildConfig');
         t.end();
       });
@@ -105,17 +104,73 @@ test('create a build-config', (t) => {
   });
 });
 
-test('remove - build-config - basic remove', (t) => {
+test('update - buildconfig', (t) => {
   const settings = {
     configLocation: `${__dirname}/test-config`
   };
 
   openshiftConfigLoader(settings).then((config) => {
     openshiftRestClient(config).then((client) => {
-      t.equal(typeof client.buildconfigs.remove, 'function', 'There is a remove method on the build configs object');
+      t.equal(typeof client.buildconfigs.create, 'function', 'There is a create method on the buildconfigs object');
 
       const clientConfig = privates.get(client).config;
-      const buildConfigName = 'cool-deployment-name-1';
+      const buildconfig = {
+        kind: 'BuildConfig'
+      };
+      const buildConfigName = 'cool-buildconfig-name-1';
+
+      nock(clientConfig.cluster)
+        .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
+        .put(`/oapi/v1/namespaces/${clientConfig.context.namespace}/buildconfigs/${buildConfigName}`)
+        .reply(200, {kind: 'BuildConfig'});
+
+      const createResult = client.buildconfigs.update(buildConfigName, buildconfig).then((buildconfig) => {
+        t.equal(buildconfig.kind, 'BuildConfig', 'returns an object with BuildConfig');
+        t.end();
+      });
+
+      t.equal(createResult instanceof Promise, true, 'should return a Promise');
+    });
+  });
+});
+
+test('remove - buildconfigs - basic removeAll', (t) => {
+  const settings = {
+    configLocation: `${__dirname}/test-config`
+  };
+
+  openshiftConfigLoader(settings).then((config) => {
+    openshiftRestClient(config).then((client) => {
+      t.equal(typeof client.buildconfigs.removeAll, 'function', 'There is a removeAll method on the buildconfigs object');
+
+      const clientConfig = privates.get(client).config;
+
+      nock(clientConfig.cluster)
+        .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
+        .delete(`/oapi/v1/namespaces/${clientConfig.context.namespace}/buildconfigs`)
+        .reply(200, {kind: 'Status'});
+
+      const removeResult = client.buildconfigs.removeAll().then((buildConfigList) => {
+        t.equal(buildConfigList.kind, 'Status', 'returns an object with Status');
+        t.end();
+      });
+
+      t.equal(removeResult instanceof Promise, true, 'should return a Promise');
+    });
+  });
+});
+
+test('remove - buildconfigs - basic remove', (t) => {
+  const settings = {
+    configLocation: `${__dirname}/test-config`
+  };
+
+  openshiftConfigLoader(settings).then((config) => {
+    openshiftRestClient(config).then((client) => {
+      t.equal(typeof client.buildconfigs.remove, 'function', 'There is a remove method on the buildconfigs object');
+
+      const clientConfig = privates.get(client).config;
+      const buildConfigName = 'cool-buildconfig-name-1';
 
       nock(clientConfig.cluster)
         .matchHeader('authorization', `Bearer ${clientConfig.user.token}`) // taken from the config
@@ -132,7 +187,7 @@ test('remove - build-config - basic remove', (t) => {
   });
 });
 
-test('remove - build-configs - remove - no build config name', (t) => {
+test('remove - buildconfigs - remove - no buildconfig name', (t) => {
   const settings = {
     configLocation: `${__dirname}/test-config`
   };
