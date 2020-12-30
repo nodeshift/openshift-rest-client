8db342036063b2dd7ba73e4e2f207f704036d4a5
Mikel Sanchez <misanche@redhat.com>
2020-12-30 15:51:46 +0100

    fix!: Removed authUrl added automatic auth url retrieval.
    * This removes the need to specify a url when doing basic auth.  The module will now pull the correct authorization endpoint from the cluster.
    
    * The current flow was a little broken and confusing.  Since this removes an option,  it will be a BREAKING CHANGE

--- test/basic-auth-request-test.js
+++ test/basic-auth-request-test.js
@@ -7,6 +7,12 @@ const proxyquire = require('proxyquire');
 
 test('basic auth request', (t) => {
   const basicAuthRequest = proxyquire('../lib/basic-auth-request', {
+    './authorization-server-request': {
+      getAuthUrlFromOCP: (url, insecureSkipTlsVerify) => {
+        t.equal(url, 'http://');
+        return Promise.resolve('https://');
+      }
+    },
     request: (requestObject, cb) => {
       t.equal(requestObject.strictSSL, false, 'should be false');
       return cb(null, {
@@ -37,8 +43,94 @@ test('basic auth request', (t) => {
   });
 });
 
+test('basic auth request with 404 status code', (t) => {
+  const basicAuthRequest = proxyquire('../lib/basic-auth-request', {
+    './authorization-server-request': {
+      getAuthUrlFromOCP: (url, insecureSkipTlsVerify) => {
+        t.equal(url, 'http://');
+        return Promise.resolve('https://');
+      }
+    },
+    request: (requestObject, cb) => {
+      t.equal(requestObject.strictSSL, false, 'should be false');
+      return cb(null, {
+        statusCode: 404,
+        request: {
+          uri: {
+            host: 'https://'
+          }
+        }
+      });
+    }
+  });
+
+  const settings = {
+    url: 'http://',
+    user: 'username',
+    password: 'password',
+    insecureSkipTlsVerify: true
+  };
+
+  const p = basicAuthRequest.getTokenFromBasicAuth(settings);
+
+  t.equal(p instanceof Promise, true, 'is an Promise');
+
+  p.catch((error) => {
+    t.equal(error.message,
+      'Unable to authenticate user username to https://. Cannot obtain access token from response.',
+      'should be equal');
+    t.end();
+  });
+});
+
+test('basic auth request with 401 status code', (t) => {
+  const basicAuthRequest = proxyquire('../lib/basic-auth-request', {
+    './authorization-server-request': {
+      getAuthUrlFromOCP: (url, insecureSkipTlsVerify) => {
+        t.equal(url, 'http://');
+        return Promise.resolve('https://');
+      }
+    },
+    request: (requestObject, cb) => {
+      t.equal(requestObject.strictSSL, false, 'should be false');
+      return cb(null, {
+        statusCode: 401,
+        request: {
+          uri: {
+            host: 'https://'
+          }
+        }
+      });
+    }
+  });
+
+  const settings = {
+    url: 'http://',
+    user: 'username',
+    password: 'password',
+    insecureSkipTlsVerify: true
+  };
+
+  const p = basicAuthRequest.getTokenFromBasicAuth(settings);
+
+  t.equal(p instanceof Promise, true, 'is an Promise');
+
+  p.catch((error) => {
+    t.equal(error.message,
+      '401 Unable to authenticate user username',
+      'should be equal');
+    t.end();
+  });
+});
+
 test('basic auth request with request error', (t) => {
   const basicAuthRequest = proxyquire('../lib/basic-auth-request', {
+    './authorization-server-request': {
+      getAuthUrlFromOCP: (url, insecureSkipTlsVerify) => {
+        t.equal(url, undefined);
+        return Promise.resolve('https://');
+      }
+    },
     request: (requestObject, cb) => {
       return cb({ message: 'Error' }, {});
     }
@@ -52,96 +144,6 @@ test('basic auth request with request error', (t) => {
 
   p.catch((error) => {
     t.equal(error.message, 'Error', 'should be equal');
-    t.end();
-  });
-});
-
-test('basic auth request with 401 error', (t) => {
-  const basicAuthRequest = proxyquire('../lib/basic-auth-request', {
-    request: (requestObject, cb) => {
-      return cb(null, {
-        statusCode: 401
-      });
-    }
-  });
-
-  const settings = {
-    user: 'username'
-  };
-
-  const p = basicAuthRequest.getTokenFromBasicAuth(settings);
-
-  t.equal(p instanceof Promise, true, 'is an Promise');
-
-  p.catch((error) => {
-    t.equal(error.message, '401 Unable to authenticate user username', 'should be equal');
-    t.end();
-  });
-});
-
-test('basic auth request with defined auth url', (t) => {
-  const basicAuthRequest = proxyquire('../lib/basic-auth-request', {
-    request: (requestObject, cb) => {
-      t.true(requestObject.url.includes('https://test'), 'Unexpected auth url value');
-      return cb(null, {
-        statusCode: 200,
-        request: {
-          uri: {
-            hash: '#access_token=9jXMEO87d7Rtf6FVQTFjumwIDbGeMzAtr2U010Z_ZG0&expires_in=86400&scope=user%3Afull&token_type=Bearer'
-          }
-        }
-      });
-    }
-  });
-
-  const settings = {
-    url: 'http://',
-    authUrl: 'https://test',
-    user: 'username',
-    password: 'password',
-    insecureSkipTlsVerify: true
-  };
-
-  const p = basicAuthRequest.getTokenFromBasicAuth(settings);
-
-  t.equal(p instanceof Promise, true, 'is an Promise');
-
-  p.then((token) => {
-    t.equal(token, '9jXMEO87d7Rtf6FVQTFjumwIDbGeMzAtr2U010Z_ZG0', 'should be equal');
-    t.end();
-  });
-});
-
-test('basic auth request with missing hash', (t) => {
-  const basicAuthRequest = proxyquire('../lib/basic-auth-request', {
-    request: (requestObject, cb) => {
-      t.true(requestObject.url.includes('https://test'), 'Unexpected auth url value');
-      return cb(null, {
-        statusCode: 200,
-        request: {
-          uri: {
-            hash: undefined,
-            host: 'testhost'
-          }
-        }
-      });
-    }
-  });
-
-  const settings = {
-    url: 'http://',
-    authUrl: 'https://test',
-    user: 'username',
-    password: 'password',
-    insecureSkipTlsVerify: true
-  };
-
-  const p = basicAuthRequest.getTokenFromBasicAuth(settings);
-
-  t.equal(p instanceof Promise, true, 'is an Promise');
-
-  p.catch((error) => {
-    t.equal(error.message, 'Unable to authenticate user username to testhost. Cannot obtain access token from response.', 'should be equal');
     t.end();
   });
 });
