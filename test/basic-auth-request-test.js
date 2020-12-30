ee9ed2893b12dbf5cd5ec1df51b3dcc36fb44cf0
Mikel Sanchez <misanche@redhat.com>
2020-12-30 15:51:46 +0100

    Removed authUrl added automatic auth url retrieval

--- test/basic-auth-request-test.js
+++ test/basic-auth-request-test.js
@@ -16,7 +16,8 @@ test('basic auth request', (t) => {
             hash: '#access_token=9jXMEO87d7Rtf6FVQTFjumwIDbGeMzAtr2U010Z_ZG0&expires_in=86400&scope=user%3Afull&token_type=Bearer'
           }
         }
-      });
+      },
+      '{"authorization_endpoint": "http://"}');
     }
   });
 
@@ -52,96 +53,6 @@ test('basic auth request with request error', (t) => {
 
   p.catch((error) => {
     t.equal(error.message, 'Error', 'should be equal');
-    t.end();
-  });
-});
-
-test('basic auth request with 401 error', (t) => {
-  const basicAuthRequest = proxyquire('../lib/basic-auth-request', {
-    request: (requestObject, cb) => {
-      return cb(null, {
-        statusCode: 401
-      });
-    }
-  });
-
-  const settings = {
-    user: 'username'
-  };
-
-  const p = basicAuthRequest.getTokenFromBasicAuth(settings);
-
-  t.equal(p instanceof Promise, true, 'is an Promise');
-
-  p.catch((error) => {
-    t.equal(error.message, '401 Unable to authenticate user username', 'should be equal');
-    t.end();
-  });
-});
-
-test('basic auth request with defined auth url', (t) => {
-  const basicAuthRequest = proxyquire('../lib/basic-auth-request', {
-    request: (requestObject, cb) => {
-      t.true(requestObject.url.includes('https://test'), 'Unexpected auth url value');
-      return cb(null, {
-        statusCode: 200,
-        request: {
-          uri: {
-            hash: '#access_token=9jXMEO87d7Rtf6FVQTFjumwIDbGeMzAtr2U010Z_ZG0&expires_in=86400&scope=user%3Afull&token_type=Bearer'
-          }
-        }
-      });
-    }
-  });
-
-  const settings = {
-    url: 'http://',
-    authUrl: 'https://test',
-    user: 'username',
-    password: 'password',
-    insecureSkipTlsVerify: true
-  };
-
-  const p = basicAuthRequest.getTokenFromBasicAuth(settings);
-
-  t.equal(p instanceof Promise, true, 'is an Promise');
-
-  p.then((token) => {
-    t.equal(token, '9jXMEO87d7Rtf6FVQTFjumwIDbGeMzAtr2U010Z_ZG0', 'should be equal');
-    t.end();
-  });
-});
-
-test('basic auth request with missing hash', (t) => {
-  const basicAuthRequest = proxyquire('../lib/basic-auth-request', {
-    request: (requestObject, cb) => {
-      t.true(requestObject.url.includes('https://test'), 'Unexpected auth url value');
-      return cb(null, {
-        statusCode: 200,
-        request: {
-          uri: {
-            hash: undefined,
-            host: 'testhost'
-          }
-        }
-      });
-    }
-  });
-
-  const settings = {
-    url: 'http://',
-    authUrl: 'https://test',
-    user: 'username',
-    password: 'password',
-    insecureSkipTlsVerify: true
-  };
-
-  const p = basicAuthRequest.getTokenFromBasicAuth(settings);
-
-  t.equal(p instanceof Promise, true, 'is an Promise');
-
-  p.catch((error) => {
-    t.equal(error.message, 'Unable to authenticate user username to testhost. Cannot obtain access token from response.', 'should be equal');
     t.end();
   });
 });
