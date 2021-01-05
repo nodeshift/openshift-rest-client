50ae2e45bc4eaf6793e57a2cf38d63d21612107a
Mikel Sanchez <misanche@redhat.com>
2021-01-05 09:30:52 +0100

    Updated tests

--- test/basic-auth-request-test.js
+++ test/basic-auth-request-test.js
@@ -38,6 +38,70 @@ test('basic auth request', (t) => {
   });
 });
 
+test('basic auth request with empty body', (t) => {
+  const basicAuthRequest = proxyquire('../lib/basic-auth-request', {
+    request: (requestObject, cb) => {
+      t.equal(requestObject.strictSSL, false, 'should be false');
+      return cb(null, {
+        statusCode: 200,
+        request: {
+          uri: {
+            hash: '#access_token=9jXMEO87d7Rtf6FVQTFjumwIDbGeMzAtr2U010Z_ZG0&expires_in=86400&scope=user%3Afull&token_type=Bearer'
+          }
+        }
+      },
+      '{}');
+    }
+  });
+
+  const settings = {
+    url: 'http://',
+    user: 'username',
+    password: 'password',
+    insecureSkipTlsVerify: true
+  };
+
+  const p = basicAuthRequest.getTokenFromBasicAuth(settings);
+
+  t.equal(p instanceof Promise, true, 'is an Promise');
+
+  p.catch((error) => {
+    t.equal(error.message,
+      'Unable to retrieve the token_endpoint for undefined. Cannot obtain token_endpoint from response.',
+      'should be equal');
+    t.end();
+  });
+});
+
+test('basic auth request with 404 status code', (t) => {
+  const basicAuthRequest = proxyquire('../lib/basic-auth-request', {
+    request: (requestObject, cb) => {
+      t.equal(requestObject.strictSSL, false, 'should be false');
+      return cb(null, {
+        statusCode: 404
+      });
+    }
+  });
+
+  const settings = {
+    url: 'http://',
+    user: 'username',
+    password: 'password',
+    insecureSkipTlsVerify: true
+  };
+
+  const p = basicAuthRequest.getTokenFromBasicAuth(settings);
+
+  t.equal(p instanceof Promise, true, 'is an Promise');
+
+  p.catch((error) => {
+    t.equal(error.message,
+      '404 Unable to get the auth url',
+      'should be equal');
+    t.end();
+  });
+});
+
 test('basic auth request with request error', (t) => {
   const basicAuthRequest = proxyquire('../lib/basic-auth-request', {
     request: (requestObject, cb) => {
